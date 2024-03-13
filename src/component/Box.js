788a1cc67e69edad9c0911bda7ff304a7241ffb8
geumhee <rmagml9769@gmail.com>
2024-03-13 17:09:05 +0900

    feat: {`box ${result}`} 클래스명에 따른 css 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Box = (props) => {
-  let result
+  let result;
   if (props.title === "computer" && props.result!=="tie"&& props.result!=="") {
-    result = props.result ==="win" ? "lose" : "win"
+    result = props.result ==="win" ? "lose" : "win";
   } else{
-    result = props.result
+    result = props.result;
   }
-console.log("result",result)
   return (
-    <div className="box" >
-      <h2>{props.title}</h2>
+    <div className={`box ${result}`} >
+      <h1>{props.title}</h1>
+      <h2 data-testid="item-name">{props.item && props.item.name}</h2>
       <img className="item-img" src={props.item && props.item.img}/>
       <h2>{result}</h2>
     </div>
